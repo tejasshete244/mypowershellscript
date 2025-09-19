2b5c5dfc11c0e4bb8dd02ea2332be5a4a5429290
tejasshete244 <112416946+tejasshete244@users.noreply.github.com>
2025-09-19 17:57:27 +0530

    Update nvidia_fabric.sh

--- nvidia_fabric.sh
+++ nvidia_fabric.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 set -ex
 systemctl enable nvidia-fabricmanager
+
+TMPDIR=$(mktemp -d)
+cd $TMPDIR
+curl -O https://efa-installer.amazonaws.com/aws-efa-installer-1.41.0.tar.gz
+tar -xf aws-efa-installer-1.41.0.tar.gz
+cd aws-efa-installer
+./efa_installer.sh -y -n
+cd /tmp/
+rm -rf $TMPDIR
+modinfo efa
